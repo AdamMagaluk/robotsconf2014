bef71622900b134f931d44079aa0ad2803efd1eb
Adam Magaluk <AdamMagaluK@gmail.com>
2014-12-06 13:30:14 -0500

    Added spark to demo

--- PinoccioZetta/server.js
+++ PinoccioZetta/server.js
@@ -1,7 +1,9 @@
 var zetta = require('zetta');
 var Pinoccio = require('zetta-pinoccio-api-driver');
+var Spark = require('zetta-spark-http-driver');
 
 zetta()
   .use(Pinoccio, process.env.PINOCCIO_USER, process.env.PINOCCIO_PASS)
+  .use(Spark, process.env.SPARK_USER, process.env.SPARK_PASS)
   .link('http://hello-zetta.herokuapp.com')
   .listen(1337);
